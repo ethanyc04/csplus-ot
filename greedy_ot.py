aeaf49fc5ee894d1606a9ee6804d62cc4e68c954
Ethan Chen <ethan.chen@duke.edu>
2024-05-28 15:05:06 -0400

    leaf node case

--- greedy_ot.py
+++ greedy_ot.py
@@ -8,4 +8,12 @@ def is_leaf(qtree):
         return False
 
 def compute_ot(qtree, cost_func):
-    
+    if is_leaf(qtree): # there should only be one point 
+        p = qtree.square.points[0]
+        val = min(p.data)
+        if val > 0: # if both distributions have mass at same point
+            transport_plan[(p.x, p.y)] = ((p.x, p.y), val)
+            p.data = [m-val for m in p.data]
+        if max(p.data) > 0: # if there is still mass at point push up
+            return qtree.square.points
+        else: return []
